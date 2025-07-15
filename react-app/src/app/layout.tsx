7c541b5882ab283c7e3193cbcfd207093472086e
David Schultz <21699112+david-schultz@users.noreply.github.com>
2025-07-15 16:06:30 -0700

    Serialization issues are due to local fonts

--- react-app/src/app/layout.tsx
+++ react-app/src/app/layout.tsx
@@ -54,26 +54,26 @@ const exposure = localFont({
   variable: '--font-exposure'
 });
 
-const pressura = localFont({ 
-  src: [
-    {
-      path: '../fonts/Pressura/GT-Pressura-Light-Trial.otf',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Pressura/GT-Pressura-Regular-Trial.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Pressura/GT-Pressura-Bold-Trial.otf',
-      weight: '800',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-pressura'
-});
+// const pressura = localFont({ 
+//   src: [
+//     {
+//       path: '../fonts/Pressura/GT-Pressura-Light-Trial.otf',
+//       weight: '200',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../fonts/Pressura/GT-Pressura-Regular-Trial.otf',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../fonts/Pressura/GT-Pressura-Bold-Trial.otf',
+//       weight: '800',
+//       style: 'normal',
+//     },
+//   ],
+//   variable: '--font-pressura'
+// });
 
 const pressuraMono = localFont({ 
   src: [
@@ -96,24 +96,24 @@ const pressuraMono = localFont({
   variable: '--font-pressura-mono'
 });
 
-const inter = Inter({ subsets: ['latin'] })
-const graphik = localFont({
-  src: [
-    {
-      path: '../fonts/Graphik/GraphikRegular.otf',
-      weight: '400'
-    },
-    {
-      path: '../fonts/Graphik/GraphikMedium.otf',
-      weight: '500'
-    },
-    {
-      path: '../fonts/Graphik/GraphikSemibold.otf',
-      weight: '600'
-    },
-  ],
-  variable: '--font-graphik'
-})
+// const inter = Inter({ subsets: ['latin'] })
+// const graphik = localFont({
+//   src: [
+//     {
+//       path: '../fonts/Graphik/GraphikRegular.otf',
+//       weight: '400'
+//     },
+//     {
+//       path: '../fonts/Graphik/GraphikMedium.otf',
+//       weight: '500'
+//     },
+//     {
+//       path: '../fonts/Graphik/GraphikSemibold.otf',
+//       weight: '600'
+//     },
+//   ],
+//   variable: '--font-graphik'
+// })
 
 export const metadata: Metadata = {
   title: 'David Schultz',
